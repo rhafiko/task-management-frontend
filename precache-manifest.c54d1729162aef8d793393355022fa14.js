799e63a57ace4ef310d81f5f9cfb7645f1c1b469
rhafiko <rhafiko@users.noreply.github.com>
2021-07-19 23:49:44 +0000

    deploy: 81f1df7b2626c52d98014a04ce4ce6c56afa19da

---
+++ precache-manifest.c54d1729162aef8d793393355022fa14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006a50c25756579ebdaecd05a01094f7",
+    "url": "https://rhafiko.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "5d8b4f2290562f5ddf8d",
+    "url": "https://rhafiko.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0b0c7d4486e912a082a2",
+    "url": "https://rhafiko.github.io/task-management-frontend/static/js/2.b1c731a9.chunk.js"
+  },
+  {
+    "revision": "5d8b4f2290562f5ddf8d",
+    "url": "https://rhafiko.github.io/task-management-frontend/static/js/main.733c23a0.chunk.js"
+  },
+  {
+    "revision": "77d832af156f6e94541a",
+    "url": "https://rhafiko.github.io/task-management-frontend/static/js/runtime~main.d253c9d3.js"
+  }
+]);
